839577495d395112e52ddec4fa8457e1fb91cd5b
Matt117 <matt.beddows@hotmail.co.uk>
2026-05-07 10:39:04 +0100

    Add markdown viewers, firmware flashing, download manager, UI improvements

--- A4IM/run_with_ssl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Force older TLS versions and disable TLS 1.3
-export GIT_SSL_GNUTLS_PRIORITY="NORMAL:-VERS-TLS1.3:+VERS-TLS1.2:+VERS-TLS1.1:+VERS-TLS1.0"
-# Configure git to use this SSL version
-git config --global http.sslVersion tlsv1.2
-# Run the program
-./run_gitfilereader.sh
